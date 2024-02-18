df901ba2a1b44fc3effff89eac77b43a6e2644b5
karanrajput2003 <97956908+karanrajput2003@users.noreply.github.com>
2024-02-18 05:59:34 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -59,7 +59,8 @@ require('./app/routes/user.routes')(app);
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome" });
+  // res.json({ message: "Welcome" });
+  res.sendFile(__dirname + '/home.html');
 });
 
 app.get("/login", (req, res) => {
@@ -79,17 +80,111 @@ app.get("/api/auth/home", (req, res) => {
   res.render("userhome", {foundUser});
 });
 
-app.get("/api/auth/doctorhome", (req, res) => {
-  res.render("doctorhome", {foundUser});
-});
+const db1 = require("./app/models");
+const Patients = db1.patient;
+const User = db1.user;
 
+app.get("/api/auth/consultStatus", async (req, res) => {
+
+  try {
+    
+    const user = foundUser.id;
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    const patients = await Patients.find({ id: user._id });
+
+    res.render("userstatus", { patients });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 app.get("/api/auth/pastconsult", (req, res) => {
   res.render("userpastconsultation", {foundUser});
 });
-app.get("/api/auth/consultnow", (req, res) => {
-  res.render("consultnow", {foundUser});
+
+app.get("/api/auth/doctorhome", (req, res) => {
+  res.render("doctorhome", {foundUser});
 });
+app.get("/api/auth/consultnow", (req, res) => {
+    res.render("consultnow", { foundUser });
+});
+// app.get("/api/auth/pastconsultdoctor", (req, res) => {
+//   res.render("pastconsultdoctor", {foundUser});
+// });
+app.get("/api/auth/doctorprofile", (req, res) => {
+  res.render("doctorprofile", {foundUser});
+});
+
+
+app.get("/api/auth/consultpatient", async (req, res) => {
+  try {
+    const patients = await Patients.find({ approved: "No" });
+
+    res.render("consultpatient", { patients });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+  // res.render("consultpatient", {foundUser});
+});
+
+app.get("/api/auth/pastconsultdoctor", async (req, res) => {
+  try {
+    const patients = await Patients.find({ approved: "Yes" });
+
+    res.render("pastconsultdoctor", { patients });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+  // res.render("consultpatient", {foundUser});
+});
+
+app.get("/api/auth/viewdetails", async (req, res) => {
+  try {
+    const userId = req.query.id;
+    console.log(userId);
+    if(!userId) {
+      console.log("error");
+    }
+    const patients = await Patients.find({ _id: userId });
+
+    res.render("viewdetails", { patients });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+  // res.render("consultpatient", {foundUser});
+});
+
+app.get("/api/auth/approve", async (req, res) => {
+  try {
+    const userId = req.query.id;
+    console.log(userId);
+    if(!userId) {
+      console.log("error");
+    }
+    // const patients = await Patients.find({ _id: userId });
+    const patients = await Patients.findByIdAndUpdate(
+      userId,
+      { approved: "Yes" }
+    );
+    if (!patients) {
+      return res.status(404).send("Patient not found");
+    }
+
+    res.render("doctorhome", { patients });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+  // res.render("consultpatient", {foundUser});
+});
+
+
 app.get("/api/auth/userprofile", (req, res) => {
   res.render("userprofile", {foundUser});
 
